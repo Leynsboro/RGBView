a70f3b91bb4519f65d33775e27de6f4f262fb118
Ilya <leynsboro@gmail.com>
2022-04-10 07:47:21 +0300

    Added and created all function

--- RGBView/ViewController.swift
+++ RGBView/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var hudFilmSpizdili: UILabel!
+    
+    @IBOutlet var redNumber: UILabel!
+    @IBOutlet var greenNumber: UILabel!
+    @IBOutlet var blueNumber: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    @IBOutlet var colorizedView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redNumber.text = String(redSlider.value)
+        greenNumber.text = String(greenSlider.value)
+        blueNumber.text = String(blueSlider.value)
+        
+        settingViewColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+        
+        hudFilmSpizdili.text = "storyboard смело украл, но в свое оправдание скажу, что  сделал абсолютно так же, без указания ширины внутренних стэков, из-за чего у меня не получалось"
     }
 
+    @IBAction func redSliderAction() {
+        redNumber.text = String(format:"%.2f", redSlider.value)
+        settingViewColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenNumber.text = String(format:"%.2f", greenSlider.value)
+        settingViewColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+    }
+    
+    
+    @IBAction func blueSliderAction() {
+        blueNumber.text = String(format:"%.2f", blueSlider.value)
+        settingViewColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+    }
+}
 
+extension ViewController {
+    private func settingViewColor(red: Float, green: Float, blue: Float) {
+        colorizedView.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+        
+    }
 }
 
