96ff0c310d71037ab9cf6d6bb0f58357f4dd0425
Ilya <leynsboro@gmail.com>
2022-04-17 16:53:39 +0300

    Some fixes

--- RGBView/ViewController.swift
+++ RGBView/ViewController.swift
@@ -9,9 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-    @IBOutlet var hudFilmSpizdili: UILabel!
-    
     @IBOutlet var redNumber: UILabel!
     @IBOutlet var greenNumber: UILabel!
     @IBOutlet var blueNumber: UILabel!
@@ -20,43 +17,39 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
     @IBOutlet var colorizedView: UIView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        redNumber.text = String(redSlider.value)
-        greenNumber.text = String(greenSlider.value)
-        blueNumber.text = String(blueSlider.value)
-        
-        settingViewColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
-        
-        hudFilmSpizdili.text = "storyboard смело украл, но в свое оправдание скажу, что  сделал абсолютно так же, без указания ширины внутренних стэков, из-за чего у меня не получалось"
-    }
 
-    @IBAction func redSliderAction() {
-        redNumber.text = String(format:"%.2f", redSlider.value)
-        settingViewColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+        setValue()
+        setViewColor()
     }
     
-    @IBAction func greenSliderAction() {
-        greenNumber.text = String(format:"%.2f", greenSlider.value)
-        settingViewColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
-    }
-    
-    
-    @IBAction func blueSliderAction() {
-        blueNumber.text = String(format:"%.2f", blueSlider.value)
-        settingViewColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+    @IBAction func sliderChange() {
+        setValue()
+        setViewColor()
     }
 }
 
 extension ViewController {
-    private func settingViewColor(red: Float, green: Float, blue: Float) {
-        colorizedView.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
-        
+    private func setViewColor() {
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue() {
+        redNumber.text = setFormatOf(value: redSlider.value)
+        greenNumber.text = setFormatOf(value: greenSlider.value)
+        blueNumber.text = setFormatOf(value: blueSlider.value)
+    }
+    
+    private func setFormatOf(value: Float) -> String {
+        String(format: "%.2f", value)
     }
 }
 
